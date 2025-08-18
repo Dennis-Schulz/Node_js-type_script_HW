be77d7b1126ae1a3151aa78c60671b6876737723
Dennis Schulz <denissulc401@gmail.com>
2025-08-18 18:44:20 +0200

    homework 13 added

---
+++ HW_13/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database connected");
+    } catch (error) {
+        console.log("Database connection error", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
